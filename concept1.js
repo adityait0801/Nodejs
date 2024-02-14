caa8d18da699efbe164b5319a1611a316baa031a
Aditya Singh <aditya.it0801@gmail.com>
2024-02-14 11:58:59 +0530

    Destructuring Concepts

--- concept1.js
+++ concept1.js
@@ -1,7 +1,52 @@
+// 1 way of concept 
+
+// console.log("Calculate");
+
+// const x = require('./concept2')
+
+// const sum = x[0];
+// const product = x[1];
+
+// const result1 = sum(6,3);
+// const result2 = product(6,3);
+
+// console.log(result1);
+
+// console.log(result2); 
+
+// console.log("Calculate");
+
+// const [sum, product] = require('./concept2')
+
+// const result1 = sum(6,3);
+// const result2 = product(6,3);
+
+// console.log(result1);
+
+// console.log(result2);
+
+// if we reverse the const [product] = require('./concept2') then output will be 5 because from the importing file the sum is at index 0 so thayt we will be exporting and will be recievd at the imorting end.
+
+// 2nd way 
+
+// console.log("Calculate");
+
+// const {product, sum} = require('./concept2')
+
+// const result1 = sum(6,3);
+// const result2 = product(6,3);
+
+// console.log(result1);
+
+// console.log(result2);
+
 console.log("Calculate");
 
-var sum = require('./concept2')
+const {product, sum} = require('./concept2')
 
-const result = sum(6,3);
+const result1 = sum(6,3);
+const result2 = product(6,3);
 
-console.log(result);
+console.log(result1);
+
+console.log(result2);
--- concept2.js
+++ concept2.js
@@ -2,4 +2,12 @@ const sum = (a, b) => {
  return a+b;
 }
 
-module.exports = sum;
+const product = (a, b) => {
+    return a*b;
+   }
+
+//    module.exports = sum;
+
+// module.exports = [sum, product];
+
+module.exports = {sum, product};
