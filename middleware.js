ef6407521a945573f98d7884dea1773fc708e84b
Aditya Singh <aditya.it0801@gmail.com>
2024-02-15 19:40:48 +0530

    Middleware for Calculating the time taken by Website

--- middleware.js
+++ middleware.js
@@ -1,20 +1,31 @@
 const express = require('express')
+const fs = require('fs')
 
 const app = express()
 
-const logger= (req, res, next) => {
-    console.log(req.method+" "+req.url);
+const timer= (req, res, next) => {
+    const startTime = new Date().getTime();
     next();
+    const endTime = new Date().getTime();
+    console.log(req.method+" "+req.url)
+    console.log(endTime - startTime);
 }
 
-app.use(logger);
+app.use(timer);
 
 app.get("/", (req, res)=> {
     res.send("This the home page");
 })
 
 app.get("/details", (req, res)=> {
-    res.send("Details are below");
+    fs.readFile("./posts", "utf-8", (err, data)=>{
+        if(err)
+        {
+            return res.status(500).send(err);
+        }
+        res.status(200).send(data);
+    })
+    // res.send("Details are below");
 })
 
 app.listen(8500, ()=> {
