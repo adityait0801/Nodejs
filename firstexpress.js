870980b1be0580e3337493ba1308cbcd07d14917
Aditya Singh <aditya.it0801@gmail.com>
2024-02-15 10:38:15 +0530

    Method+ Route

--- firstexpress.js
+++ firstexpress.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express()
 
+app.get("/", (req, res)=> {
+    res.send("this the home page");
+})
+
+app.get("/about", (req, res)=> {
+    res.send("We are about to start");
+})
+
+
 app.listen(8000, () => {
     console.log("listening pn port 8000")
     })
