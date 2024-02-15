a66c44896aa46ed4dcaabe95cfc34f8aa468b9d4
Aditya Singh <aditya.it0801@gmail.com>
2024-02-15 11:33:57 +0530

    Requests using Express

--- firstexpress.js
+++ firstexpress.js
@@ -1,13 +1,33 @@
-const express = require('express');
+const express = require('express')
+const fs = require('fs')
 
 const app = express()
+app.use(express.text())
+app.use(express.json())
 
 app.get("/", (req, res)=> {
     res.send("this the home page");
 })
 
 app.get("/about", (req, res)=> {
+    console.log(req.url+" "+req.method);
+    res.send("We are about to start"); //
+})
+
+app.post("/about", (req, res)=> {
+    console.log(req.url+" "+req.method);
+    console.log(req.body);
     res.send("We are about to start");
+})
+
+app.get("/posts", (req, res)=> {
+    fs.readFile("./posts.json", "utf-8", (err, posts)=>{
+        if(err)
+        {
+            return res.send(err);
+        }
+        res.send(posts);
+    })
 })
 
 
