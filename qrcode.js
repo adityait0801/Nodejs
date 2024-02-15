4587ed8a7ebccd2ce4d92c9e318c7292943aa322
Aditya Singh <aditya.it0801@gmail.com>
2024-02-15 10:22:07 +0530

    Starting Express

--- qrcode.js
+++ qrcode.js
@@ -1,5 +1,5 @@
 const qr = require("qrcode");
 
-qr.toFile("qr.png", "https://nodejs.org/docs/latest/api/"); 
+qr.toFile("qr.png", "https://colonist.io/#cSrw"); 
         //"save qr code to image file", "path text"
 
