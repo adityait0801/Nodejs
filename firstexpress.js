86d0a5e906d428f17417e17da944c4434d247277
Aditya Singh <aditya.it0801@gmail.com>
2024-02-15 12:51:07 +0530

    Params Request

--- firstexpress.js
+++ firstexpress.js
@@ -36,6 +36,29 @@ app.get("/welcome", (req, res)=> {
     res.send("Welcome"+" "+user )
 })
 
+app.get("/details", (req, res)=> {
+    console.log(req.query);
+    // const name = req.query.name;
+    // const age = req.query.age;
+
+    const {name, age} = req.query;
+
+    if(age>=18)
+    {
+        res.send("Details You are"+" "+name+" "+"is Eligible");
+    }
+    else
+    {
+        res.send("Details you are"+" "+name+" "+"is not Eligible");
+    }
+})
+
+app.get("/lecture/:id", (req,res)=> {
+    console.log(req.params);
+    const number = req.params.id;
+    res.send("Lecture"+" "+number);
+})
+
 
 app.listen(8000, () => {
     console.log("listening on port 8000")
