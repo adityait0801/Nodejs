4df4280718b77a299c773e9e81696dd06cb170c3
Aditya Singh <aditya.it0801@gmail.com>
2024-02-15 19:09:10 +0530

    Middleware for Monitoring the URL & Method

--- middleware.js
+++ middleware.js
@@ -2,15 +2,16 @@ const express = require('express')
 
 const app = express()
 
-const random_character_middleware = () => {
-    console.log("Hi from Middleware");
+const logger= (req, res, next) => {
+    console.log(req.method+" "+req.url);
+    next();
 }
+
+app.use(logger);
 
 app.get("/", (req, res)=> {
     res.send("This the home page");
 })
-
-app.use(random_character_middleware);
 
 app.get("/details", (req, res)=> {
     res.send("Details are below");
