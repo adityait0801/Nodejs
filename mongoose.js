68e59cc23ac02c409c24069ed0db19b6d6b4caa7
Aditya Singh <aditya.it0801@gmail.com>
2024-02-17 16:33:32 +0530

    Mongoose Model & Schema

--- mongoose.js
+++ mongoose.js
@@ -4,9 +4,25 @@ require('dotenv').config();
                                  //("URL of the MongoDB/database_name")
 
 const main = async () => {
+
+    const userSchema = mongoose.Schema({
+        name : String,
+        city : String
+    }) 
+
+    const user_model = mongoose.model("user", userSchema);
+       // user_model is acting as a collection in MongoDB concept 
+
     try{
     const connection = await mongoose.connect(process.env.Mongo_URL);
+        //this will connect to the DB S/w and the DB File inside it.
     console.log("Connected to DB Successfully");
+
+        await user_model.insertMany([{name : "Aditya", city :"Raipur"},{name : "Adi", city : "Rpr"}]);
+            // the above will act as a document as i MongoDB concept.
+        //since it may take time to insert data in DB beacuse of slow internet or xyz reasons so we will put inside the async operation 
+        console.log("Inserted data successfully");
+
     connection.disconnect();
     }
     catch(err)
