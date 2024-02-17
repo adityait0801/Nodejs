57fa363fb530c6290c2d9ef59f0e79047c736e77
Aditya Singh <aditya.it0801@gmail.com>
2024-02-17 18:28:37 +0530

    Inserting & Reading the data using Mongoose & Express

---
+++ mern.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const {connection, User_model} = require('./mongoose')
+
+const app = express();
+
+app.get('/', (req, res)=> {
+    res.send("api working, base route");
+})
+
+app.get('/users', async (req, res)=> {
+    const users = await User_model.find({});
+    res.send("users");
+})
+
+app.post('/users', async (req, res)=> {
+    const user = req.body; //to get the data from the user
+    console.log(user);
+    await User_model.insertMany([user]);
+    res.send({msg : "user inserted successfully", user});
+})
+
+//for inserting one document
+app.post('/users', async (req, res)=> {
+    const payload = req.body; //to get the data from the user
+    console.log(payload);
+    const new_user = new Usermodel.insertOne(payload);
+    await new_user.save();
+    res.send({msg : "user inserted successfully", new_user});
+})
+
+app.listen(8000, async()=> {
+    try{
+    await connection
+    console.log("Connected to MongoDB Successfully");
+    }
+    catch(err)
+    {
+        console.log(err);
+        console.log("Error Connecting to DB");
+    }
+    console.log("listening on port 8000");
+})
--- mongoose.js
+++ mongoose.js
@@ -3,31 +3,16 @@ require('dotenv').config();
 //const connection = mongoose.connect("mongodb://localhost:27017/dbnodejs") 
                                  //("URL of the MongoDB/database_name")
 
-const main = async () => {
-
     const userSchema = mongoose.Schema({
         name : String,
         city : String
     }) 
 
-    const user_model = mongoose.model("user", userSchema);
+    const User_model = mongoose.model("user", userSchema);
        // user_model is acting as a collection in MongoDB concept 
 
-    try{
-    const connection = await mongoose.connect(process.env.Mongo_URL);
-        //this will connect to the DB S/w and the DB File inside it.
-    console.log("Connected to DB Successfully");
+    const connection = mongoose.connect(process.env.Mongo_URL);  
 
-        await user_model.insertMany([{name : "Aditya", city :"Raipur"},{name : "Adi", city : "Rpr"}]);
-            // the above will act as a document as i MongoDB concept.
-        //since it may take time to insert data in DB beacuse of slow internet or xyz reasons so we will put inside the async operation 
-        console.log("Inserted data successfully");
-
-    connection.disconnect();
-    }
-    catch(err)
-    {
-        console.log(err);
-    }
+module.exports = {
+    connection,  User_model
 }
-main();
